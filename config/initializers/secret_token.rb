9533916c0ef49462f790040a4413132c9de627fd
Kate Daly <kate.s.daly@gmail.com>
2013-09-15 09:36:55 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sana5::Application.config.secret_key_base = '3d3bba842fdccf57fe809c041f512cb95abe7ac4a85335853911c85ef7504145ec61e30bb5dd3ce691bb33129e2d3ce5c53faceb08d53f2fad63a0e104205c74'

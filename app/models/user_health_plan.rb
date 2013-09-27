120836feffe645d20c94cc5e598faa1bf658615a
Kate Daly <kate.s.daly@gmail.com>
2013-09-27 15:03:02 -0400

    added plan_length_days to health_plans and start_date to user_health_plans'

--- app/models/user_health_plan.rb
+++ app/models/user_health_plan.rb
@@ -3,6 +3,13 @@ class UserHealthPlan < ActiveRecord::Base
   belongs_to :user
   belongs_to :health_plan
 
+  validates_presence_of :start_date
+
+  def assign
+    self.start_date = Date.today
+    save
+  end
+
   def self.options
     HealthPlan.all
   end
--- spec/models/health_plan_spec.rb
+++ spec/models/health_plan_spec.rb
@@ -9,5 +9,7 @@ describe HealthPlan do
   it {should_not have_valid(:title).when(nil, '')}
   it {should have_valid(:description).when('string')}
   it {should_not have_valid(:description).when(nil, '')}
+  it {should have_valid(:plan_length_days).when(14, 21)}
+  it {should_not have_valid(:plan_length_days).when(nil, '')}
 
 end

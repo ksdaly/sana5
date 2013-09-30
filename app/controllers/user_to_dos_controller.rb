7d1e07688c7d061313ff5ac069de4b0bb566002e
Kate Daly <kate.s.daly@gmail.com>
2013-09-30 13:31:25 -0400

    cleanup

--- app/controllers/user_to_dos_controller.rb
+++ app/controllers/user_to_dos_controller.rb
@@ -6,11 +6,11 @@ class UserToDosController < ApplicationController
     @user_to_dos = UserToDo.where(user_id: current_user.id, day: Date.today).order(:id)
   end
 
-  def complete
-    UserToDo.where(id: params[:redo_ids], completed: true).update_all(completed: false)
-    UserToDo.where(id: params[:user_to_do_ids], completed: false).update_all(completed: true)
-    redirect_to user_to_dos_path
-  end
+  # def complete
+  #   UserToDo.where(id: params[:redo_ids], completed: true).update_all(completed: false)
+  #   UserToDo.where(id: params[:user_to_do_ids], completed: false).update_all(completed: true)
+  #   redirect_to user_to_dos_path
+  # end
 
   def update
     if @user_to_do.update(user_to_do_params)

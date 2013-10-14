904c76235af21fe4b4e68c2326210d4e16269e08
Kate Daly <kate.s.daly@gmail.com>
2013-10-14 19:28:55 -0400

    to_dos spec for multiple plans

--- spec/lib/seeders/to_dos_spec.rb
+++ spec/lib/seeders/to_dos_spec.rb
@@ -9,6 +9,7 @@ describe Seeders::ToDos do
     seeder_health_plans.seed
     seeder_to_dos.seed
     expect(ToDo.where(title: seeded_to_do)).to be_present
+    expect_multiple_health_plans
   end
 
   it 'does not create duplicates' do
@@ -19,6 +20,12 @@ describe Seeders::ToDos do
     expect(ToDo.count).to  eql(count_after_seed)
   end
 
+end
+
+def expect_multiple_health_plans
+  HealthPlan.select("id, title").group("id").each do |variable|
+    expect(ToDo.where("health_plan_id= ?", variable.id)).to be_present
+  end
 end
 
 

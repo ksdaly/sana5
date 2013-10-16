5bcb2d762c54da87e6c5231011c9fec491aad089
Kate Daly <kate.s.daly@gmail.com>
2013-10-16 18:29:07 -0400

    health profile redirects

--- app/controllers/health_profiles_controller.rb
+++ app/controllers/health_profiles_controller.rb
@@ -3,7 +3,10 @@ class HealthProfilesController < ApplicationController
 
   before_action only: [:show, :edit, :update, :destroy] do  |x| x.require_patient! @health_profile end
 
+
+
   def index
+    redirect_to_new?
     @health_profiles = HealthProfile.where(user_id: current_user.id)
   end
 
@@ -11,6 +14,7 @@ class HealthProfilesController < ApplicationController
   end
 
   def new
+    redirect_to_edit?
     @health_profile = HealthProfile.new
   end
 
@@ -42,19 +46,31 @@ class HealthProfilesController < ApplicationController
 
     private
 
-    def old_profile?
-      # @health_profile.updated_at.to_date.beginning_of_day != Time.now.utc.to_date
-      @health_profile.created_at.to_date != Time.now.utc.to_date
-    end
+  def old_profile?
+    # @health_profile.updated_at.to_date.beginning_of_day != Time.now.utc.to_date
+    @health_profile.created_at.to_date != Time.now.utc.to_date
+  end
 
-    def set_health_profile
-      @health_profile = HealthProfile.find(params[:id])
-    end
+  def set_health_profile
+    @health_profile = HealthProfile.find(params[:id])
+  end
 
-    def health_profile_params
-      params.require(:health_profile).permit(:male, :dob, :weight, :height, :systolic_bp,
-        :diastolic_bp, :antihypertensive_drugs, :steroid_drugs, :diabetes, :parent_with_diabetes,
-        :sibling_with_diabetes, :smoker, :exsmoker, :cardiovascular_risk, :diabetes_risk)
-    end
+  def health_profile_params
+    params.require(:health_profile).permit(:male, :dob, :weight, :height, :systolic_bp,
+      :diastolic_bp, :antihypertensive_drugs, :steroid_drugs, :diabetes, :parent_with_diabetes,
+      :sibling_with_diabetes, :smoker, :exsmoker, :cardiovascular_risk, :diabetes_risk)
+  end
+
+  def redirect_to_new?
+    redirect_to new_health_profile_path if no_profile?
+  end
+
+  def redirect_to_edit?
+    redirect_to edit_health_profile_path(current_user.health_profiles.last) if !no_profile?
+  end
+
+  def no_profile?
+    current_user.health_profiles.blank?
+  end
 
 end
